682841519f7b0e6bc2bb7ee2e60dbef4356d1fe3
Felix Middendorf <felix@felixmiddendorf.eu>
2009-08-09 17:54:21 +0000

    Deleted unnecessary comments, because the code is pretty self-explanatory. Sometimes less is more.

--- examples/caching.php
+++ examples/caching.php
@@ -3,16 +3,16 @@ require '..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'class.templify.php';
 
 // Configuration
 $t = new Templify();
-$t->setCaching(true);			// enable caching
-$t->setCacheLifetime(60*60);	// 1 hour
+$t->setCaching(true);
+$t->setCacheLifetime(60*60);
 
 // Rendering this page for the first time will take quite some time.
 // However, rendering this page for the second, third and quadrillion time within the next hour will be really fast
 if(!$t->isCached('print_array.php')){
-	// the code inside these curly braces is only executed if there is not a cached version
+	// the code inside these curly braces is only executed if there is no cached version
 	$data = array('hello', 'everybody', '<b>out</b>', 'there', 'have', 'fun', 0, pi(), 0x12);
 	$t->assign('data', $data);
-	usleep(1500000); // this simulates complex calculations, database queries etc...
+	usleep(1500000); // this simulates complex calculations, database queries etc
 }
 
 $t->display('print_array.php');
